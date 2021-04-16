618934fcc000afcb7776023cc966c37387870fd8
lhmisho <lokman.hossen@circlefintech.com>
2021-04-17 00:11:29 +0600

    db connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const chalk = require('chalk')
+const mongoose = require('mongoose')
 const app = express()
+
+
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.urlencoded({extended:true}))  // to accept form data
 app.use(express.json())  // to accept json data
 
@@ -10,8 +16,13 @@ app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
+const DB_URI = 'mongodb://localhost:27017/account_manager'
 const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`)
-})
-    
+mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log(chalk.green("DB connected"))
+        app.listen(PORT, () => {
+            console.log(chalk.green(`Server running on ${PORT}`))
+        })        
+    })
+    .catch(e => console.log(e))
